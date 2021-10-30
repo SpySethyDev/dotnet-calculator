b10dee57a39ea144bbbca7d10640642269d86c1d
SpySethyDev <87845069+SpySethyDev@users.noreply.github.com>
2021-10-30 11:15:28 +1100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# dotnet calculator
- A calculator created using Microsoft's Dotnet Program
+
+![Logo](https://lh3.googleusercontent.com/omYrz8aZzfIFO-5B99gYrmYm4ix3ca7CytAunzErFV2PkTOUrg6K_TPftKFkiNaNgeL9cmHIJ7AVcBkLrkokYTpmeRSh7hbvMZiOoNVReIgTnWtj7EqcmG4cZhrOqKaKyP2ZsbRvdlKPtb7uEt2CB5E4h5yjfYKcoOhmSsKsPdeNdJ-v78GoZrtEz0q7C1LV9ptmlfDSTGaqSkkqJQFr9EoixnGHoNXBWUNqft5RQBrA0sWxSEZeCaw9FcTXmfRPhXxc0aOFsGdiJC420xetXIULNwJgCmN4HRdAHF_yLHadjOGx7k0zBugIIbXGBBrfpi8PDxGsI0wQlfMdA7N0GSd2CFnpCAHKtxu4GKnKm666x-DwxVj_4sP_k5VG7dixxVa6RPq8ulcEknZXIE9pWf_MDYAGZRI43RQ5JRa6sdvTs2Z3tYW6cRRSSfsOfTkBuTAgJuz5XlK0NQxa1Wio54ntAk1UcsAl0HXB86AdzbHLYYOtUmmBy2vVjcS8TTox9PHnxGOg2JrRx0-m0g04mACMFxIhlWcRwWQwY73EF7ghsyimc0WDVrUqX5WSKapSJEccfovQilN8YI_ZTrLdeFqidLMCwE1Xiy_0ws6ktxWdDr4-lMlFkoskdD8_JU8JLAi5Ty_UKZq2RMRv5JjC127ZbZjTHSYEuBHF4j51GrVeNZR1FTG65R8-sCiLmLOn8RFGtlZE4salps5wfyn8Gaw=w1920-h392-no?authuser=0)
+
+
+# Dotnet Calculator
+
+A calculator created using Microsoft's Dotnet Program
+## Features
+
+- A fully working calculator
+- Works in .NET
+- Runs in a Command Prompt window
+
+
+## Installation
+
+Install .NET
+
+- Go to [.NET](https://dotnet.microsoft.com/download) and download the recommended SDK
+- Launch the installer and install it
+- Open the .exe file
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Authors
+
+- [@SpySethyDev](https://www.github.com/SpySethyDev)
+
---
+++ src/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1;
+            double number2;
+            double finishNum;
+            string operation;
+            string[] operations = 
+            {
+                "Addition",
+                "Subtraction",
+                "Multiplication",
+                "Division"
+            };
+
+            Console.Title = "Calculator";
+
+            Console.Write("Input a number: ");
+            number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input another number: ");
+            number2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Pick an operation:");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("["+ i + "] " + operations[i]);
+            }
+            operation = Console.ReadLine();
+
+            if (operation == "0"){
+                finishNum = number1 + number2;
+                Console.WriteLine("The finished number is: " + finishNum);
+                Console.ReadKey();
+            }
+            else if (operation == "1"){
+                finishNum = number1 - number2;
+                Console.WriteLine("The finished number is: " + finishNum);
+                Console.ReadKey();
+            }
+            else if (operation == "2"){
+                finishNum = number1 * number2;
+                Console.WriteLine("The finished number is: " + finishNum);
+                Console.ReadKey();
+            }
+            else if (operation == "3"){
+                finishNum = number1 / number2;
+                Console.WriteLine("The finished number is: " + finishNum);
+                Console.ReadKey();
+            }
+            else{
+                Console.WriteLine("That is not one of the above");
+                Console.ReadKey();
+            }
+        }
+    }
+}
